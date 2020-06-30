20e49973a50e609beb59434dfb8ab363c4b57041
Pablo Henrique <pablohdev@outlook.com>
2020-06-30 07:02:45 -0300

    first commit

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from 'typeorm';
+import { sign, verify } from 'jsonwebtoken';
+import { compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response {
+    user: User;
+    token: string;
+}
+
+class AuthenticateUserService {
+    public async execute({ email, password }: Request): Promise<Response> {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({ where: { email } });
+
+        if (!user) {
+            throw new Error('Incorrect email/password combination');
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if (!passwordMatched) {
+            throw new Error('Incorrect email/password combination');
+        }
+
+        const token = sign({}, 'c378a47a75d9c0e005906de01abda272', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token,
+        };
+    }
+}
+
+export default AuthenticateUserService;
