d41f04b6faea71ea551da3714066468f354d9d85
Pablo Henrique <pablohdev@outlook.com>
2020-09-15 00:14:39 -0300

    finished refactoring from  api using SOLID principles

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,43 @@
+import { hash } from 'bcryptjs';
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+@injectable
+class CreateUserService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+    ) { }
+
+    public async execute({ name, email, password }: IRequest): Promise<User> {
+        const checkUserExists = await this.usersRepository.findByEmail(email);
+
+        if (checkUserExists) {
+            throw new AppError('Email addres already used');
+        }
+
+        const hashedPassword = await hash(password, 8);
+
+        const user = await this.usersRepository.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        await this.usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export default CreateUserService;
---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,55 @@
+import path from 'path';
+import fs from 'fs';
+import { inject, injectable } from 'tsyringe';
+
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+    user_id: string;
+    avatarFileName: string;
+}
+@injectable
+class UpdateUserAvatarService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+    ) { }
+
+    public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
+        const user = await this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new AppError(
+                'Only authenticated users can change avatar',
+                401,
+            );
+        }
+
+        if (user.avatar) {
+            // Deletar avatar anterior
+
+            const userAvatarFilePath = path.join(
+                uploadConfig.directory,
+                user.avatar,
+            );
+            const userAvatarFileExists = await fs.promises.stat(
+                userAvatarFilePath,
+            );
+
+            if (userAvatarFileExists) {
+                await fs.promises.unlink(userAvatarFilePath);
+            }
+        }
+
+        user.avatar = avatarFileName;
+
+        await this.usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export default UpdateUserAvatarService;
